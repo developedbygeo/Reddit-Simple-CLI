db5c182e4cb6bbd1a0e6a4fa38fe600be6183057
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-14 02:06:43 +0300

    feat: updating help menu

--- settings.mjs
+++ settings.mjs
@@ -3,19 +3,24 @@ import { Command } from 'commander';
 const core = new Command();
 
 core
-.version('1.0.0')
+.version('1.0.0', '-v, --version', 'Show the version number')
 .name('Reddit-Simple-CLI')
+.usage('<command> [global options]')
 .description('A simple CLI to browse Reddit');
 
+core.helpOption('-h, --help', 'Show the help menu');
 
-core
-.command('version')
-.alias('--version')
-.description('Get the version of the program')
-.action(() => {
-    console.log(core.version())
+core.on('--help', () => {
+    console.log('');
+    console.log('  ----Examples----');
+    console.log('');
+    console.log('    $ reddit best');
+    console.log('    $ reddit hot');
+    console.log('    $ reddit new');
+    console.log('    $ reddit top');
+    console.log('    $ reddit subreddit <subredditName>');
+    console.log('    $ reddit user <username>');
+    console.log('');
 })
-
-
 
 export default core;
